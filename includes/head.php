f80a2d1a630bd39c1ac8b6b078f608c503a66d7c
CristianEduardoo <cj.94@hotmail.com>
2024-03-19 12:19:11 +0100

    zona de anuncios terminada

--- includes/head.php
+++ includes/head.php
@@ -5,5 +5,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes Raíces</title>
-    <link rel="stylesheet" href="./build/css/app.css?v=1">
+    <link rel="stylesheet" href="./build/css/app.css">
 </head>
--- index.php
+++ index.php
@@ -28,17 +28,112 @@
             </div>
             <div class="icono">
                 <img src="./build/img/icono2.svg" alt="Icono Seguridad" loading="lazy">
-                <h3>Seguridad</h3>
+                <h3>Precio</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipiscing elit neque hac tristique, a nisl purus ultrices nunc accumsan molestie etiam habitant eros curae, orci aliquam at sociosqu fames convallis pharetra ut eleifend.</p>
             </div>
             <div class="icono">
                 <img src="./build/img/icono3.svg" alt="Icono Seguridad" loading="lazy">
-                <h3>Seguridad</h3>
+                <h3>A Tiempo</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipiscing elit neque hac tristique, a nisl purus ultrices nunc accumsan molestie etiam habitant eros curae, orci aliquam at sociosqu fames convallis pharetra ut eleifend.</p>
             </div>
         </div>
-
     </main>
+    <section class="seccion contenedor">
+        <h2>Casas y Departamentos en venta</h2>
+        <div class="contenedor-anuncios">
+            <div class="anuncio"> <!-- anuncio 1 -->
+                <picture>
+                    <source srcset="./build/img/anuncio1.webp" type="image/webp">
+                    <img loading="lazy" width="200" height="300" src="./build/img/anuncio1.jpg" alt="anuncios">
+                </picture>
+                <div class="contenido-anuncio">
+                    <h3>Casa de lujo en el lago</h3>
+                    <p>Casa en el lago con exelente vista, acabados de lujo a un exelente precio</p>
+                    <p class="precio">3,000,000€</p>
+
+                    <ul class="iconos-caracteristicas">
+                        <li>
+                            <img loading="lazy" src="./build/img/icono_wc.svg" alt="icono wc" />
+                            <p>4</p>
+                        </li>
+                        <li>
+                            <img loading="lazy" src="./build/img/icono_estacionamiento.svg" alt="icono estacionamiento" />
+                            <p>1</p>
+                        </li>
+                        <li>
+                            <img loading="lazy" src="./build/img/icono_dormitorio.svg" alt="icono hanitaciones" />
+                            <p>4</p>
+                        </li>
+                    </ul>
+
+                    <a href="anuncio.php" class="boton btn-amarillo">Ver propiedad</a>
+                </div>
+            </div> <!-- fin anuncio 1 -->
+            <div class="anuncio"> <!-- anuncio 2-->
+                <picture>
+                    <source srcset="./build/img/anuncio2.webp" type="image/webp">
+                    <img loading="lazy" width="200" height="300" src="./build/img/anuncio2.jpg" alt="anuncios">
+                </picture>
+                <div class="contenido-anuncio">
+                    <h3>Casa Centrica en Madrid</h3>
+                    <p>Casa con diseño moderno, así como una buena domotica y amueblada
+                    </p>
+                    <p class="precio">5,000,000€</p>
+
+                    <ul class="iconos-caracteristicas">
+                        <li>
+                            <img loading="lazy" src="./build/img/icono_wc.svg" alt="icono wc" />
+                            <p>4</p>
+                        </li>
+                        <li>
+                            <img loading="lazy" src="./build/img/icono_estacionamiento.svg" alt="icono estacionamiento" />
+                            <p>2</p>
+                        </li>
+                        <li>
+                            <img loading="lazy" src="./build/img/icono_dormitorio.svg" alt="icono hanitaciones" />
+                            <p>4</p>
+                        </li>
+                    </ul>
+
+                    <a href="anuncio.php" class="boton btn-amarillo">Ver propiedad</a>
+                </div>
+            </div> <!-- fin anuncio 2-->
+            <div class="anuncio"> <!-- anuncio 3 -->
+                <picture>
+                    <source srcset="./build/img/anuncio3.webp" type="image/webp">
+                    <img loading="lazy" width="200" height="300" src="./build/img/anuncio3.jpg" alt="anuncios">
+                </picture>
+                <div class="contenido-anuncio">
+                    <h3>Hermosa casa de lujo</h3>
+                    <p>Casa con piscina y acabados de lujo en la ciudad, exelente oportunidad</p>
+                    <p class="precio">4,000,000€</p>
+
+                    <ul class="iconos-caracteristicas">
+                        <li>
+                            <img loading="lazy" src="./build/img/icono_wc.svg" alt="icono wc" />
+                            <p>5</p>
+                        </li>
+                        <li>
+                            <img loading="lazy" src="./build/img/icono_estacionamiento.svg" alt="icono estacionamiento" />
+                            <p>3</p>
+                        </li>
+                        <li>
+                            <img loading="lazy" src="./build/img/icono_dormitorio.svg" alt="icono hanitaciones" />
+                            <p>4</p>
+                        </li>
+                    </ul>
+
+                    <a href="anuncio.php" class="boton btn-amarillo">Ver propiedad</a>
+                </div>
+            </div> <!-- fin anuncio 3 -->
+        </div> <!-- contenedor-anuncios -->
+        <div class="ver-todas">
+            <a href="anuncios.php" class="boton btn-verde">Ver todas</a>
+        </div>
+    </section>
+
+
+
     <?php include './includes/footer.php' ?>
 </body>
 
