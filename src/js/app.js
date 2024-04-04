c8ca57585d71c3e6714fe7c6794b1b1cebda398c
CristianEduardoo <cj.94@hotmail.com>
2024-04-04 23:01:59 +0200

    terminando-darkMode

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,48 @@
-document.addEventListener('DOMContentLoaded', function(){
-    eventListeners();
-    darkmode();
+document.addEventListener("DOMContentLoaded", function () {
+  // Llamamos a la función que agrega los event listeners
+  eventListeners();
+  // Agregamos el manejo del modo oscuro
+  darkMode();
+  // Aplicamos el modo oscuro si está activo en el almacenamiento local
+  applyDarkModeFromLocalStorage();
+  
 });
 
-function darkmode(){
-    const btnDarkMode = document.querySelector(".dark-mode-btn");
+function darkMode() {
+  
+  const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-    btnDarkMode.addEventListener('click', activarDarkMode);
-}
-
-function activarDarkMode(){
-    document.body.classList.toggle('dark-mode');
-}
-
-function eventListeners(){
-    const mobileMenu = document.querySelector('.mobile-menu');
-
-    mobileMenu.addEventListener('click', navegacionResponsive)
-};
-
-function navegacionResponsive(){
-    const navegacion = document.querySelector('.navegacion-header');
-
-    navegacion.classList.toggle('mostrar');
+  btnDarkMode.addEventListener("click", function () {
+    // Toggle de la clase 'dark-mode' en el body
+    document.body.classList.toggle("dark-mode");
     // if(navegacion.classList.contains('mostrar')){
     //     navegacion.classList.remove('mostrar');
     // }else {
     //     navegacion.classList.add('mostrar');
     // }
+    // Guardamos el estado del modo oscuro en el almacenamiento local
+    const isDarkModeEnabled = document.body.classList.contains("dark-mode");
+    localStorage.setItem("darkModeEnabled", isDarkModeEnabled);
+  });
+}
+
+function eventListeners() {
+  // Seleccionamos el botón de menú móvil
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  mobileMenu.addEventListener("click", function () {
+
+    const navegacion = document.querySelector(".navegacion-header");
+    // Toggle de la clase 'mostrar' en la navegación
+    navegacion.classList.toggle("mostrar");
+  });
+}
+
+function applyDarkModeFromLocalStorage() {
+  // Verificamos si el modo oscuro está activado en el almacenamiento local
+  const isDarkModeEnabled = localStorage.getItem("darkModeEnabled") === "true";
+  // Si el modo oscuro está activado, aplicamos la clase 'dark-mode' al body
+  if (isDarkModeEnabled) {
+    document.body.classList.add("dark-mode");
+  }
 }
